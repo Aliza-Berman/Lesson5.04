d99c29ba732c3643f652c707688214cebc0f3e3a
Aliza Berman <alizaberman46@gmail.com>
2020-05-06 15:55:39 -0400

    Add project files.

---
+++ Lesson5.04/ClientApp/src/MyFirstComponent.js
@@ -0,0 +1,12 @@
+﻿import React from 'react';
+import MySecondComponent from './MySecondComponent';
+class MyFirstComponent extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello from My First Component</h1>
+                <MySecondComponent />
+            </div>)
+    }
+}
+export default MyFirstComponent;
---
+++ Lesson5.04/ClientApp/src/MySecondComponent.js
@@ -0,0 +1,7 @@
+﻿import React from 'react';
+class MySecondComponent extends React.Component {
+    render() {
+        return <p>Hello from My Second Component</p>
+    }
+}
+export default MySecondComponent;
